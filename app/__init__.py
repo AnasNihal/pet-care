0bfb6fba626e0b1a1320a02b0181b89168fb4e78
Ahammed Anas Nihal <ahmmedanasnihal@Ahmmeds-MacBook-Air.local>
2026-03-18 20:06:42 +0530

    Improved frontend deisign

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from app.config import Config
-from app.routes import auth, user, admin, main
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
 
 def create_app():
     app = Flask(__name__)
@@ -15,12 +19,18 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
     login_manager.login_message = 'Please log in to access this page.'
+
+    from app.routes import auth, user, admin, main
     
     # Register blueprints
-    app.register_blueprint(main)
-    app.register_blueprint(auth)
-    app.register_blueprint(user)
-    app.register_blueprint(admin)
+    app.register_blueprint(main.main)
+    app.register_blueprint(auth.auth)
+    app.register_blueprint(user.user)
+    app.register_blueprint(admin.admin)
+
+    with app.app_context():
+        from app import models  # make sure models are registered
+        db.create_all()
     
     # Register 404 error handler
     @app.errorhandler(404)
